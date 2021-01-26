e9d7be65ba859e9d1a01f93726cb828701be9698
quidwhiffle <jadaimani2013@gmail.com>
2021-01-25 21:55:02 -0500

    fixed the folder move thing

--- consolidate.py
+++ consolidate.py
@@ -3,9 +3,10 @@ from pathlib import Path
 sauce = r'D:\downloads'
 ignore = ['exes', 'pdfs', 'word_docs', 'ppts']
 folder = 'folderFolder'
+
 p = Path(sauce)
 for i in os.scandir(p):
-    if  i.is_dir() and i.name not in ignore:
-        shutil.move(f'{sauce}\{i}', f'{sauce}\{folder}')
+    if  i.is_dir() or i.endswith('.zip')and i.name not in ignore:
+        shutil.move(f'{sauce}\{i.name}', f'{sauce}\{folder}')
 
 #todo tomorrow: strip the direntry off somehow???
